ce583223c5adfbacae00f5e7d3ac3eb13e50c8d4
BMarx981 <b.marx981@gmail.com>
2024-06-08 08:23:15 -0400

    Add user class.

---
+++ lib/src/data/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final String? name;
+  final int id;
+  final String? userName;
+  final String password;
+  final String email;
+
+  User(this.name, this.id, this.userName, this.password, this.email);
+}
